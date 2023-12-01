7707536b6a39ad14d2ae39d9baab008e298a9ee8
Jordan Medina <jordanmedina@jordans-mbp.lan>
2023-12-01 15:30:47 -0800

    [refactor] Clean up BattleData more (move helpers in battle.gd -> battle_data.gd)

--- scenes/battle/scripts/battle.gd
+++ scenes/battle/scripts/battle.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 
-var battle_data : BattleData
+var data : BattleData
 
 
 #=======================
@@ -9,72 +9,7 @@ var battle_data : BattleData
 #=======================
 func _init() -> void:
 	var seed_data = SeedData.get_seed_data()
-
-	var lead_data : BattleArenaLeadData
-	lead_data = get_battle_arena_lead_data(seed_data)
-
-	var enemies_data = BattleArenaEnemiesData.new([
-		BattleArenaEnemyData.new(EnemyArchive.get_random_enemy_name())
-	])
-	var hand_data = BattleFieldHandData.new([])
-	
-	var deck_data : BattleFieldDeckData
-	deck_data = get_battle_field_deck_data(seed_data)
-	
-	var swap_data : BattleFieldSwapData
-	swap_data = get_battle_field_swap_data(seed_data)
-
-	var discard_data = BattleFieldDiscardData.new([])
-	var essence_data = BattleFieldEssenceData.new([])
-	battle_data = BattleData.new(
-		lead_data,
-		enemies_data,
-		hand_data,
-		deck_data,
-		swap_data,
-		discard_data,
-		essence_data
-	)
-
+	data = BattleData.create(seed_data)
 
 func _ready() -> void:
-	BattleRadio.emit_signal("battle_started", battle_data)
-
-
-#=======================
-# Data Helpers
-#=======================
-func get_battle_arena_lead_data(seed_data : Dictionary) -> BattleArenaLeadData:	
-	var character_seed_data : Dictionary = seed_data[SeedData.CHARACTERS]
-
-	# Juno starts off leading the party! :3
-	return BattleArenaLeadData.new(
-		character_seed_data[CharacterArchive.JUNO_CHARACTER]
-	)
-
-func get_battle_field_deck_data(seed_data : Dictionary) -> BattleFieldDeckData:
-	var card_seed_data : Dictionary = seed_data[SeedData.CARDS]
-#	{
-#		"juno_character": [{ # juno card array }],
-#		"pettol_character": [{ # pettol card array }],
-#		"axo_character": [{ # axo card array }],
-#	}
-	var cards_data : Array[Dictionary] = []
-
-	# iterate through all character cards
-	for character_name in card_seed_data.keys():
-		var character_cards_data : Array[Dictionary]
-		character_cards_data = card_seed_data[character_name]
-		for character_card_data in character_cards_data:
-			cards_data.append(character_card_data)
-
-	return BattleFieldDeckData.new(cards_data)
-
-func get_battle_field_swap_data(seed_data : Dictionary) -> BattleFieldSwapData:
-	var character_seed_data : Dictionary = seed_data[SeedData.CHARACTERS]
-	
-	return BattleFieldSwapData.new(
-		character_seed_data[CharacterArchive.JUNO_CHARACTER],
-		character_seed_data[CharacterArchive.AXO_CHARACTER],
-		character_seed_data[CharacterArchive.PETTOL_CHARACTER]
-	)
+	BattleRadio.emit_signal("battle_started", data)
--- scenes/battle/scripts/battle_data.gd
+++ scenes/battle/scripts/battle_data.gd
@@ -27,3 +27,69 @@ func _init(
 	swap_data = _swap_data
 	discard_data = _discard_data
 	essence_data = _essence_data
+
+static func create(seed_data : Dictionary) -> BattleData:
+	return BattleData.new(
+		get_battle_arena_lead_data(seed_data),
+		get_battle_arena_enemies_data(seed_data),
+		get_battle_field_hand_data(seed_data),
+		get_battle_field_deck_data(seed_data),
+		get_battle_field_swap_data(seed_data),
+		get_battle_field_discard_data(seed_data),
+		get_battle_field_essence_data(seed_data)
+	)
+
+
+#=======================
+# Data Helpers
+#=======================
+static func get_battle_arena_lead_data(seed_data : Dictionary) -> BattleArenaLeadData:	
+	var character_seed_data : Dictionary = seed_data[SeedData.CHARACTERS]
+
+	# Juno starts off leading the party! :3
+	return BattleArenaLeadData.new(
+		character_seed_data[CharacterArchive.JUNO_CHARACTER]
+	)
+
+static func get_battle_arena_enemies_data(_seed_data : Dictionary) -> BattleArenaEnemiesData:
+	return BattleArenaEnemiesData.new([
+		BattleArenaEnemyData.new(
+			EnemyArchive.get_random_enemy_name()
+		)
+	])
+
+static func get_battle_field_deck_data(seed_data : Dictionary) -> BattleFieldDeckData:
+	var card_seed_data : Dictionary = seed_data[SeedData.CARDS]
+#	{
+#		"juno_character": [{ # juno card array }],
+#		"pettol_character": [{ # pettol card array }],
+#		"axo_character": [{ # axo card array }],
+#	}
+	var cards_data : Array[Dictionary] = []
+
+	# iterate through all character cards
+	for character_name in card_seed_data.keys():
+		var character_cards_data : Array[Dictionary]
+		character_cards_data = card_seed_data[character_name]
+		for character_card_data in character_cards_data:
+			cards_data.append(character_card_data)
+
+	return BattleFieldDeckData.new(cards_data)
+
+static func get_battle_field_hand_data(_seed_data : Dictionary) -> BattleFieldHandData:
+	return BattleFieldHandData.new([])
+
+static func get_battle_field_swap_data(seed_data : Dictionary) -> BattleFieldSwapData:
+	var character_seed_data : Dictionary = seed_data[SeedData.CHARACTERS]
+	
+	return BattleFieldSwapData.new(
+		character_seed_data[CharacterArchive.JUNO_CHARACTER],
+		character_seed_data[CharacterArchive.AXO_CHARACTER],
+		character_seed_data[CharacterArchive.PETTOL_CHARACTER]
+	)
+
+static func get_battle_field_discard_data(_seed_data : Dictionary) -> BattleFieldDiscardData:
+	return BattleFieldDiscardData.new([])
+
+static func get_battle_field_essence_data(_seed_data : Dictionary) -> BattleFieldEssenceData:
+	return BattleFieldEssenceData.new([])

6e3e74a44767730d51eefb1f5215707ea04d1671
Jordan Medina <jordanwmedina@gmail.com>
2023-12-04 18:39:11 -0800

    [scene] Show number of cards in discard pile with label

--- scenes/battle_field_discard/scripts/battle_field_discard.gd
+++ scenes/battle_field_discard/scripts/battle_field_discard.gd
@@ -16,6 +16,7 @@ var image_data : ImageData = ImageData.new(
 #=======================
 func _init() -> void:
 	BattleRadio.connect(BattleRadio.BATTLE_STARTED, _on_battle_started)
+	BattleRadio.connect(BattleRadio.CARD_PLAYED, _on_card_played)
 
 
 #=======================
@@ -24,9 +25,20 @@ func _init() -> void:
 func set_discard_pile(new_discard_pile : Array[Card]) -> void:
 	discard_pile = new_discard_pile
 
+	$Area2D/Sprite2D/Panel/Label.update_discard_pile_number(discard_pile.size())
+
 
 #========================
 # Signal Handlers
 #========================
 func _on_battle_started(battle_data : BattleData) -> void:
 	discard_pile = battle_data.discard_pile
+
+func _on_card_played(played_card : Card, _targets : Array) -> void:
+	var new_discard_pile : Array[Card] = []
+
+	for card_in_discard_pile in self.discard_pile:
+		new_discard_pile.append(card_in_discard_pile)
+	new_discard_pile.append(played_card)
+
+	discard_pile = new_discard_pile
